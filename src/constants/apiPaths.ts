3cff351812e69a12a643b94f2e4a6452bf0a48fc
faichuk <volodymyr.faichuk@goto.com>
2023-03-26 13:50:47 +0300

    Task 7: Authorization

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://42rjyvfjp1.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://o24r5tbdv2.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,19 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
+
+const getErrorMessageByStatusCode = (statusCode: number) => {
+  if (statusCode === 403) {
+    return "403 Forbidden!";
+  }
+
+  if (statusCode === 401) {
+    return "401 Unauthorized!";
+  }
+
+  return "Unexpected error!";
+};
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,9 +32,20 @@ if (import.meta.env.DEV) {
   worker.start({ onUnhandledRequest: "bypass" });
 }
 
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const statusCode = error.response.status;
+    const errorMessage = getErrorMessageByStatusCode(statusCode);
+    alert(errorMessage);
+    return Promise.reject(error.response);
+  }
+);
+
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
